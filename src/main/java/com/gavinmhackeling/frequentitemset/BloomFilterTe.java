e29383969ab401585467ddab163db403b9b983f3
gavinmh <gavinhackeling@gmail.com>
2013-10-11 00:41:03 -0400

    wip

--- src/main/java/com/gavinmhackeling/frequentitemset/BloomFilterTe.java
+++ src/main/java/com/gavinmhackeling/frequentitemset/BloomFilterTe.java
@@ -1,5 +1,8 @@
 package com.gavinmhackeling.frequentitemset;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
 import com.google.common.hash.BloomFilter;
 
 public class BloomFilterTe {
@@ -9,7 +12,34 @@ public class BloomFilterTe {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int minimumSupport = 3;
 		StringFunnel stringFunnel = new StringFunnel();
+		BloomFilter<String>[] filters = new BloomFilter[minimumSupport];
+		for (int i=0; i < filters.length; i++)
+		{
+			filters[i] = BloomFilter.create(stringFunnel, 1000); // TODO decrease number of expected insertions each time
+		}
+		
+		List<String> tokens = Lists.newArrayList("a", "b", "c", "d", "a", "a", "b");
+		
+		for (String token: tokens)
+		{
+			int greatestFilter = 0;
+			for (int i=0; i < filters.length; i++)
+			{
+				greatestFilter = i;
+				if (!filters[i].mightContain(token))
+					break;
+			}
+			System.out.println("the greatest filter for " + token + " is " + greatestFilter);
+			filters[greatestFilter].put(token);
+//			if (!filters[0].mightContain(token))
+//			{
+//				filters[0].put(token);
+//			}
+		}
+		
+		
 		BloomFilter<String> bloomFilter = BloomFilter.create(stringFunnel, 1000); 
 		
 		bloomFilter.put("a");
